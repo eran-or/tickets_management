9443e6514e1c727130ab7cebd2d2c82f80dacb23
eranor <e.or1976@gmail.com>
2019-02-07 21:08:07 +0200

    Updates

---
+++ precache-manifest.f23329f7bfd75c7f1e6e490cffe4926d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ba8d7b5dce1a41de0956",
+    "url": "/tickets_management/static/js/runtime~main.ba8d7b5d.js"
+  },
+  {
+    "revision": "9ce54e8f3fb6b72d5ee2",
+    "url": "/tickets_management/static/js/main.9ce54e8f.chunk.js"
+  },
+  {
+    "revision": "16f4f90e72d1039ec070",
+    "url": "/tickets_management/static/js/1.16f4f90e.chunk.js"
+  },
+  {
+    "revision": "9ce54e8f3fb6b72d5ee2",
+    "url": "/tickets_management/static/css/main.96d5def0.chunk.css"
+  },
+  {
+    "revision": "16f4f90e72d1039ec070",
+    "url": "/tickets_management/static/css/1.ac612994.chunk.css"
+  },
+  {
+    "revision": "7c6e49dfc38b0461ba08b7128050560b",
+    "url": "/tickets_management/index.html"
+  }
+];
